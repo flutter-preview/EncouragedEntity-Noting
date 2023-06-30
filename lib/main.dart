41eca2bb60ac1f4a3aebce35cb07eba23b8f6d44
Artur <74054908+EncouragedEntity@users.noreply.github.com>
2023-06-30 08:52:40 +0300

    AuthService'd main

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,9 @@
 // ignore_for_file: prefer_const_constructors, avoid_printRegisterView
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:noting/constants/colors.dart';
+import 'package:noting/services/auth/auth_service.dart';
 import 'package:noting/widgets/all_widgets.dart';
 import 'Views/all_views.dart';
-import 'firebase_options.dart';
 import 'constants/routes.dart';
 // ignore: unused_import
 import 'dart:developer' as devtools show log;
@@ -50,16 +48,17 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    final authService = AuthService.firebase();
+
     return FutureBuilder(
-      future: Firebase.initializeApp(
-        options: DefaultFirebaseOptions.currentPlatform,
-      ),
+      future: authService.initialize(),
       builder: (context, snapshot) {
         switch (snapshot.connectionState) {
           case ConnectionState.done:
-            final user = FirebaseAuth.instance.currentUser;
+            final user = authService.currentUser;
             if (user != null) {
-              if (user.emailVerified) {
+              if (user.isEmailVerified) {
                 return const NotesView();
               } else {
                 return const VerifyEmailView();
