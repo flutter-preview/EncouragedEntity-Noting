22dfa01a4756f5bb5cfc13c19586faa2820005f4
Artur <74054908+EncouragedEntity@users.noreply.github.com>
2023-06-30 11:04:16 +0300

    Auth tests

--- test/auth_test.dart
+++ test/auth_test.dart
@@ -1,7 +1,79 @@
+import 'package:noting/services/auth/auth_exception.dart';
+import 'package:noting/services/auth/auth_provider.dart';
+import 'package:noting/services/auth/auth_user.dart';
+import 'package:test/test.dart';
 
+void main() {}
 
+class NotInitializedException implements Exception {}
 
-void main()
-{
-  
+class MockAuthProvider implements AuthProvider {
+  AuthUser? _user;
+  var _isInitialized = false;
+  bool get isInitialized => _isInitialized;
+
+  @override
+  Future<AuthUser> createUser({
+    required String email,
+    required String password,
+  }) async {
+    if (!isInitialized) {
+      throw NotInitializedException();
+    }
+    await Future.delayed(const Duration(seconds: 1));
+    return logIn(
+      email: email,
+      password: password,
+    );
+  }
+
+  @override
+  AuthUser? get currentUser => _user;
+  @override
+  Future<void> initialize() async {
+    await Future.delayed(const Duration(seconds: 1));
+    _isInitialized = true;
+  }
+
+  @override
+  Future<AuthUser> logIn({
+    required String email,
+    required String password,
+  }) {
+     if (!isInitialized) {
+      throw NotInitializedException();
+    }
+    if(email == "foo") throw UserNotFoundException();
+    if(password == "bar") throw WrongPasswordException();
+    const user = AuthUser(isEmailVerified: false);
+    _user = user;
+    return Future.value(user);
+  }
+
+  @override
+  Future<void> logOut() async{
+     if (!isInitialized) {
+       throw NotInitializedException();
+     }
+
+    if(_user == null) {
+      throw UserNotFoundException();
+    }
+
+    await Future.delayed(const Duration(seconds: 1));
+    _user = null;
+  }
+
+  @override
+  Future<void> sendEmailVerification() async{
+     if (!isInitialized) {
+       throw NotInitializedException();
+     }
+    final user = _user;
+    if(user == null)
+    {
+      throw UserNotFoundException();
+    }
+    _user = const AuthUser(isEmailVerified: true);
+  }
 }
